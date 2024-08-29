f57114d35d69b1f3e391978669557df7f638bfa2
Presha Karan <159997342+preshaakaran@users.noreply.github.com>
2024-08-30 02:28:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+gsap.from(".one",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:60,
+})
+
+gsap.from(".two",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:60,
+})
+
+gsap.from(".three",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:-60,
+})
+
+gsap.from("#name",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+})
